6113c4ecc721da76a12b5ce7f7caa9d33b9e76ac
Ilya Bylich <ibylich@gmail.com>
2021-03-05 19:44:54 +0300

    always create build directory

--- scripts/static-lib-to-object-darwin.sh
+++ scripts/static-lib-to-object-darwin.sh
@@ -1,5 +1,6 @@
 pwd
 
+rm -rf extracted
 mkdir -p extracted
 cp $RUST_LIB_FILE extracted/
 cd extracted
@@ -7,4 +8,3 @@ RUST_FILE=$(basename $RUST_LIB_FILE)
 ar dv $RUST_FILE apple_versioning.o
 ld -arch x86_64 -r -force_load ${RUST_FILE} -o $RUST_OBJ_FILE
 cd ..
-rm -rf extracted
