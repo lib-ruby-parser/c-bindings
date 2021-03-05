9d9cc92cf4d27b2142f273eabc00cb9098820f1c
Ilya Bylich <ibylich@gmail.com>
2021-03-05 19:40:46 +0300

    rewrap rust static lib into obj file

---
+++ scripts/static-lib-to-object-darwin.sh
@@ -0,0 +1,10 @@
+pwd
+
+mkdir -p extracted
+cp $RUST_LIB_FILE extracted/
+cd extracted
+RUST_FILE=$(basename $RUST_LIB_FILE)
+ar dv $RUST_FILE apple_versioning.o
+ld -arch x86_64 -r -force_load ${RUST_FILE} -o $RUST_OBJ_FILE
+cd ..
+rm -rf extracted
