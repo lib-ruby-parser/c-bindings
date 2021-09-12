401e3c1e0028bd97e0c197a45b8c20bf326c4713
Ilya Bylich <ibylich@gmail.com>
2021-09-12 22:21:11 +0300

    removed unused clang attributes

--- alloc.c
+++ alloc.c
@@ -1,11 +1,11 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-void *__attribute__((always_inline)) lib_ruby_parser_allocate(uint32_t size)
+void *lib_ruby_parser_allocate(uint32_t size)
 {
     return malloc(size);
 }
-void __attribute__((always_inline)) lib_ruby_parser_free(void *ptr)
+void lib_ruby_parser_free(void *ptr)
 {
     free(ptr);
 }
