8000696ada9e7570dec6ad34276a63d74e13ff84
Ilya Bylich <ibylich@gmail.com>
2021-11-15 18:57:21 +0300

    remove debug output

--- string.c
+++ string.c
@@ -8,10 +8,8 @@
 
 char *new_owned_string(const char *s)
 {
-    fprintf(stderr, "new_owned_string called\n");
     char *out = (char *)malloc(strlen(s) + 1);
     strcpy(out, s);
-    fprintf(stderr, "new_owned_string completed\n");
     return out;
 }
 
@@ -19,32 +17,26 @@ LIB_RUBY_PARSER_String lib_ruby_parser__test__make_string_foo();
 
 test(rust_string_fields, {
     LIB_RUBY_PARSER_String foo = lib_ruby_parser__test__make_string_foo();
-    fprintf(stderr, "Constructed rust string\n");
     assert(foo.len == 3);
     assert_str_eq(foo.ptr, "foo", 3);
     // capacity doesn't matter
     LIB_RUBY_PARSER_drop_string(&foo);
-    fprintf(stderr, "finishing rust string test\n");
 });
 
 test(owned_string_fields, {
     LIB_RUBY_PARSER_String foo = LIB_RUBY_PARSER_new_string_owned(new_owned_string("foo"), 3);
-    fprintf(stderr, "Constructed owned string\n");
     assert(foo.len == 3);
     assert_str_eq(foo.ptr, "foo", 3);
     // capacity doesn't matter
     LIB_RUBY_PARSER_drop_string(&foo);
-    fprintf(stderr, "finishing owned string test\n");
 });
 
 test(copied_string_fields, {
     LIB_RUBY_PARSER_String foo = LIB_RUBY_PARSER_new_string_from_cstr("foo");
-    fprintf(stderr, "Constructed copied string\n");
     assert(foo.len == 3);
     assert_str_eq(foo.ptr, "foo", 3);
     // capacity doesn't matter
     LIB_RUBY_PARSER_drop_string(&foo);
-    fprintf(stderr, "finishing copied string test\n");
 });
 
 declare_test_group(string, 3, rust_string_fields, owned_string_fields, copied_string_fields);
