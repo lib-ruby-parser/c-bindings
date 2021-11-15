c895f61c4c4e640055235f1b9842c4d9b7858372
Ilya Bylich <ibylich@gmail.com>
2021-11-15 18:51:23 +0300

    enable whole CI matrix again; add test for rust-allocated string

--- ruby-parser-c/src/string.rs
+++ ruby-parser-c/src/string.rs
@@ -5,21 +5,21 @@ blob_type!(BlobString, String);
 #[cfg(feature = "tests")]
 #[no_mangle]
 pub extern "C" fn LIB_RUBY_PARSER_new_string_owned(ptr: *mut u8, len: usize) -> BlobString {
-    eprintln!("Constructing string owned");
-
     let s = unsafe { String::from_raw_parts(ptr, len, len) };
-    eprintln!("Got {:?}", s);
     BlobString::from(s)
 }
 
 #[cfg(feature = "tests")]
 #[no_mangle]
 pub extern "C" fn LIB_RUBY_PARSER_new_string_from_cstr(ptr: *const i8) -> BlobString {
-    eprintln!("Constructing string from cstr");
-
     let s = unsafe { std::ffi::CStr::from_ptr(ptr) };
-    eprintln!("Got {:?}", s);
     BlobString::from(s.to_str().unwrap_or_default().to_owned())
+}
+
+#[cfg(feature = "tests")]
+#[no_mangle]
+pub extern "C" fn lib_ruby_parser__test__make_string_foo() -> BlobString {
+    BlobString::from(String::from("foo"))
 }
 
 #[no_mangle]
