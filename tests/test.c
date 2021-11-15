b1162b2ba2e64f33a2e9752a8eec168b65f52625
Ilya Bylich <ibylich@gmail.com>
2021-11-15 17:33:25 +0300

    try asan with mingw

--- tests/test.c
+++ tests/test.c
@@ -10,18 +10,18 @@ int main()
     printf("sizeof(void*): %d\n", (int)(sizeof(void *)));
     printf("\n");
 
-    // run_test_group(bytes);
-    // run_test_group(comment);
-    // // run_test_group(decoder);
-    // // run_test_group(diagnostic);
-    // run_test_group(loc);
-    // run_test_group(magic_comment);
-    // // run_test_group(parser_options);
-    // // run_test_group(parser_result);
-    // run_test_group(shared_byte_list);
-    // run_test_group(source_line);
-    // run_test_group(string);
-    // // run_test_group(token_rewriter);
-    // run_test_group(token);
+    run_test_group(bytes);
+    run_test_group(comment);
+    // run_test_group(decoder);
+    // run_test_group(diagnostic);
+    run_test_group(loc);
+    run_test_group(magic_comment);
+    // run_test_group(parser_options);
+    // run_test_group(parser_result);
+    run_test_group(shared_byte_list);
+    run_test_group(source_line);
+    run_test_group(string);
+    // run_test_group(token_rewriter);
+    run_test_group(token);
     return 0;
 }
