ca7b01c9ffa05de5cf36d9e61858b87be9322b36
Ilya Bylich <ibylich@gmail.com>
2021-11-16 21:04:51 +0300

    simplify string assertions in several places

--- decoder.c
+++ decoder.c
@@ -52,8 +52,7 @@ static void test_decoder_result_fields(void)
     result = lib_ruby_parser__test__make_decoder_result__ok(
         LIB_RUBY_PARSER_new_bytes_from_cstr("bar", 3));
     assert_eq(result.tag, LIB_RUBY_PARSER_DECODER_RESULT_OK);
-    assert_eq(result.as.ok.len, 3);
-    assert_str_eq(result.as.ok.ptr, "bar");
+    assert_byte_list(result.as.ok, "bar");
     LIB_RUBY_PARSER_drop_decoder_result(&result);
 
     result = lib_ruby_parser__test__make_decoder_result__err(
@@ -79,15 +78,13 @@ static void test_decoder_fields(void)
     d = lib_ruby_parser__test__always_ok_decoder();
     decoder_result = (d.f)(LIB_RUBY_PARSER_new_string_from_cstr("utf-8"), LIB_RUBY_PARSER_new_bytes_from_cstr("2 + 2", 5));
     assert_eq(decoder_result.tag, LIB_RUBY_PARSER_DECODER_RESULT_OK);
-    assert_eq(decoder_result.as.ok.len, strlen(ALWAYS_OK));
-    assert_str_eq(decoder_result.as.ok.ptr, ALWAYS_OK);
+    assert_byte_list(decoder_result.as.ok, ALWAYS_OK);
     LIB_RUBY_PARSER_drop_decoder_result(&decoder_result);
 
     d = lib_ruby_parser__test__always_err_decoder();
     decoder_result = (d.f)(LIB_RUBY_PARSER_new_string_from_cstr("utf-8"), LIB_RUBY_PARSER_new_bytes_from_cstr("2 + 2", 5));
     assert_eq(decoder_result.as.err.tag, LIB_RUBY_PARSER_INPUT_ERROR_DECODING_ERROR);
-    assert_eq(decoder_result.as.err.as.unsupported_encoding.len, strlen(ALWAYS_ERR));
-    assert_str_eq(decoder_result.as.err.as.unsupported_encoding.ptr, ALWAYS_ERR);
+    assert_string_eq(decoder_result.as.err.as.unsupported_encoding, ALWAYS_ERR);
     LIB_RUBY_PARSER_drop_decoder_result(&decoder_result);
 }
 
