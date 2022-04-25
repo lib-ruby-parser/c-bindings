b1c10a057cd74e9186b5c87f938c2dbdda0c8014
Ilya Bylich <ibylich@gmail.com>
2022-04-25 21:17:08 +0300

    use pure bash equivalent instead of basename; use clang by absolute path on ci

--- scripts/compile_obj.sh
+++ scripts/compile_obj.sh
@@ -6,13 +6,15 @@ CC=$1
 CFLAGS=$2
 INPUTS=$3
 OUTPUT=$4
+BASE_CC="${CC##*/}"
 
 echo "CC = $CC"
+echo "BASE_CC = $BASE_CC"
 echo "CFLAGS = $CFLAGS"
 echo "INPUTS = $INPUTS"
 echo "OUTPUT = $OUTPUT"
 
-if [ "$(basename \"$CC\")" = "cl.exe" ]; then
+if [ $BASE_CC = "cl.exe" ]; then
     # MSVC
     "$CC" $CFLAGS $INPUTS /link /OUT:$OUTPUT
 else
