7b58a41fe1cde1b18d60d24066a2fc4776f82a76
Ilya Bylich <ibylich@gmail.com>
2022-04-25 20:07:49 +0300

    extract custom exe compilation to a bash script

---
+++ scripts/compile_obj.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eux
+
+CC=$1
+CFLAGS=$2
+INPUTS=$3
+OUTPUT=$4
+
+echo "CC = $CC"
+echo "CFLAGS = $CFLAGS"
+echo "INPUTS = $INPUTS"
+echo "OUTPUT = $OUTPUT"
+
+if [ "$(basename $CC)" = "cl.exe" ]; then
+    # MSVC
+    $CC $CFLAGS $INPUTS /link /OUT:$OUTPUT
+else
+    # Clang/GCC/MinGW
+    $CC $CFLAGS $INPUTS -o $OUTPUT
+fi
